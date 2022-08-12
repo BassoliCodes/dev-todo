1da9481b3167ce1f95cd8d0b67182de9c4fc8443
Lucca Bassoli <lucca.bassoli2020@gmail.com>
2022-08-12 12:37:52 -0300

    feat: init project

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>teste</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(238, 238, 238)',
     alignItems: 'center',
     justifyContent: 'center',
   },
